6bb13094c61d5a73a613f801e95b34abcff1dc7e
andersonbuenos <andersonbuenos@live.com>
2024-05-14 01:05:52 -0400

    Alteração de regra para selecionar municipios recebido e municipio transferido, só serão exibidos se o status estiver com a seleção recebido e/ou transferido

--- src/app/components/data-dialog/data-dialog.component.ts
+++ src/app/components/data-dialog/data-dialog.component.ts
@@ -14,7 +14,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { ModelData } from '../../modal-data';
 import { MatTableDataSource } from '@angular/material/table';
 import { IbgeService } from '../../services/ibge.service';
-import { Observable, tap} from 'rxjs';
+import { Observable, of, tap} from 'rxjs';
 import { CommonModule } from '@angular/common';
 
 
@@ -56,8 +56,8 @@ export class DataDialogComponent {
       municipioId: ['', []],
       tipoAmpola: ['', Validators.required],
       status: ['', Validators.required],
-      municipioRecebidoId: ['', []],
-      municipioTransferidoId: ['', []],
+      municipioRecebidoId: [{value: '', disabled: false}, []],
+      municipioTransferidoId: [{value: '', disabled: false}, []],
       totalAmpolas: ['', []],
       // Defina o campo de entrada como obrigatório
     });
@@ -68,39 +68,44 @@ export class DataDialogComponent {
     }
   }
 
-  async showMunicipios(): Promise<Municipio[]> {
+  async showMunicipios(): Promise<void> {
     try {
       const data = await this.ibgeService.getMunicipiosMS();
-      return this.municipios;
+      this.municipios = data || []; // Provide a fallback value to ensure the type is always an array // Atualize a propriedade com os dados obtidos
     } catch (err) {
       console.error(err);
-      // Se ocorrer algum erro, você pode retornar um array vazio ou fazer algum tratamento adicional
-      return [];
+      this.municipios = []; // Em caso de erro, defina como array vazio
     }
   }
-
-
+  
   async ngOnInit() {
-    const municipios = await this.showMunicipios(); // Aguarde a resolução da Promise
-    this.municipios$ = new Observable<Municipio[]>(observer => {
-      observer.next(municipios);
-      observer.complete();
-    }).pipe(
-      tap((data: Municipio[]) => {
-        this.municipios = data;
-      })
-    );
+    await this.showMunicipios(); // Isso agora atualiza `this.municipios` diretamente
+    this.municipios$ = of(this.municipios); // Cria um Observable a partir de `this.municipios`
+    this.handleStatusChanges();
   }
 
-
+  handleStatusChanges(): void {
+    this.form.get('status')!.valueChanges.subscribe((status) => {
+      if (status === 'Recebido') {
+        this.form.get('municipioTransferidoId')!.disable();
+        this.form.get('municipioRecebidoId')!.enable();
+      } else if (status === 'Transferido') {
+        this.form.get('municipioRecebidoId')!.disable();
+        this.form.get('municipioTransferidoId')!.enable();
+      } else {
+        this.form.get('municipioRecebidoId')!.enable();
+        this.form.get('municipioTransferidoId')!.enable();
+      }
+    });
+  }
 
 
   onSubmit(): void {
     const formData = this.form.value;
-    let id = this.userService.getData().length + 1;
-    debugger;
-    formData.id = id;
-    this.userService.addData(formData);
+    /* let id = this.userService.getData().length + 1; */
+/*     debugger; */
+    /* formData.id = id; */
+    /* this.userService.addData(formData); */
     this.dialogRef.close(); // Feche o diálogo
   }
 }
